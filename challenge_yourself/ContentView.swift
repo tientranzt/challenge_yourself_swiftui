34e06062987b9da4cc6e3d755733c089b82f9299
tientran <tientran.home@gmail.com>
2020-12-27 19:58:48 +0700

    Initial Commit

---
+++ challenge_yourself/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  challenge_yourself
+//
+//  Created by tientran on 27/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ challenge_yourself/challenge_yourselfApp.swift
@@ -0,0 +1,17 @@
+//
+//  challenge_yourselfApp.swift
+//  challenge_yourself
+//
+//  Created by tientran on 27/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct challenge_yourselfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
